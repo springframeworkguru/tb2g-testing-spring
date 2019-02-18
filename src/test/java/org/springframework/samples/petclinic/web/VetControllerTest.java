0fab33879cf1179a639a58a7c8cf0d5248cb4809
John Thompson <john@springframework.guru>
2019-02-18 18:16:25 -0500

    adding MockMVC standalone setup example

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -34,11 +38,23 @@ class VetControllerTest {
 
     List<Vet> vetsList = new ArrayList<>();
 
+    MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
         vetsList.add(new Vet());
 
         given(clinicService.findVets()).willReturn(vetsList);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void testControllerShowVetList() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("vets"))
+            .andExpect(view().name("vets/vetList"));
     }
 
     @Test
