855b7be5ed90d414de44fba85800d1eabfbe756b
John Thompson <john@springframework.guru>
2019-02-18 15:43:56 -0500

    adding properties example

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/PropertiesWordProducer.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 2019-02-18.
+ */
+@Component
+@Profile("externalized")
+@Primary
+public class PropertiesWordProducer implements WordProducer {
+
+    private String word;
+
+    @Value("${say.word}")
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String getWord() {
+        return word;
+    }
+}
---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/PropertiesTest.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by jt on 2019-02-18.
+ */
+@TestPropertySource("classpath:yanny.properties")
+@ActiveProfiles("externalized")
+@SpringJUnitConfig(classes = PropertiesTest.TestConfig.class)
+public class PropertiesTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+
+        assertEquals("YaNNy", word);
+    }
+}
