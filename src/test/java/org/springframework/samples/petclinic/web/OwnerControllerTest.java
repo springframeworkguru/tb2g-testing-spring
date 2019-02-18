9546772f499694a13f2f51fce711c31377e0bf40
John Thompson <john@springframework.guru>
2019-02-18 18:35:03 -0500

    adding web xml config

---
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+class OwnerControllerTest {
+
+    @Autowired
+    OwnerController ownerController;
+
+    @Autowired
+    ClinicService clinicService;
+
+
+    @Test
+    void tempTest() {
+
+        //ownerController
+    }
+}
