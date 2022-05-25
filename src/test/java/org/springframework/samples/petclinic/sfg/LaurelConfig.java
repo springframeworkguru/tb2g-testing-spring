e3901c8b4f6e9e02710f39695bb0fb35f247885e
Alex <alex.rodrigues23997@gmail.com>
2022-05-25 11:25:13 -0300

    JUnit4 Laurel test

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/LaurelConfig.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class LaurelConfig {
+
+    @Bean
+    LaurelWordProducer laurelWordProducer(){
+        return new LaurelWordProducer();
+    }
+}
